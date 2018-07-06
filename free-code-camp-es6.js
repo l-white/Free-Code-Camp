3a445af0d39976c63e4249e7da9283139485e79c
Laura <law7676@outlook.com>
2018-07-06 09:49:31 -0500

    es6 challenges

---
+++ free-code-camp-es6.js
@@ -0,0 +1,24 @@
+// ES6: Compare Scopes of the var and let Keywords
+
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+
+// ES6: Declare a Read-Only Variable with the const Keyword
+
+function printManyTimes(str) {
+  "use strict";
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+}
+printManyTimes("freeCodeCamp");
